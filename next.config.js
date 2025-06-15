2a565878c65906d5e90fb1ba440da6d32d5bd993
yope7 <yope_0920@outlook.com>
2025-06-16 02:08:03 +0900

    feat: admin画面の試験的導入

--- next.config.js
+++ next.config.js
@@ -8,6 +8,12 @@ const nextConfig = {
   },
   output: 'standalone',
   distDir: '.next',
+  // ページルーティングの設定
+  pageExtensions: ['tsx', 'ts', 'jsx', 'js'],
+  // Pages Routerの設定
+  experimental: {
+    appDir: false
+  },
   // 他の設定があればここに追加
 };
 
